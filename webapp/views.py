0323ff80b400ce5d773bafc0abf8118bbc1f3a18
Eri Schon <erischon@gmail.com>
2021-03-02 09:32:43 +0100

    Update views.py

--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home_page(request):
-    return HttpResponse('<html><title>The Substitute</title></html>') 
+    return render(request, 'webapp/home.html') 
