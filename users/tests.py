98a90c07669aca94d9dc44d5cb854744947a5447
Eri Schon <erischon@gmail.com>
2021-03-04 16:27:39 +0100

    Update tests.py

--- users/tests.py
+++ users/tests.py
@@ -3,5 +3,6 @@ from django.test import TestCase
 class SignupTest(TestCase):
 
     def test_use_signup_template(self):
-        response = self.client.get('users/')
+        response = self.client.get('/users/signup/')
+        print(response)
         self.assertTemplateUsed(response, 'users/signup.html')
