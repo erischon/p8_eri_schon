ce9aceab1798e8b5e386d037a6e8ab8814f9b297
Eri Schon <erischon@gmail.com>
2021-03-08 10:47:18 +0100

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signupuser, name='signupuser'),
-    path('moncompte/', views.moncompte, name='moncompte'),  
+    path('moncompte/', views.moncompte, name='moncompte'),
+    path('login/', views.loginuser, name='loginuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),  
 ]
