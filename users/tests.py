a4b3039066a9f0894e596eb1fb2bb3dce54a1baf
Eri Schon <erischon@gmail.com>
2021-03-09 16:37:46 +0100

    Update tests.py

--- users/tests.py
+++ users/tests.py
@@ -7,3 +7,19 @@ class SignupTest(TestCase):
         response = self.client.get('/users/signup/')
         print(response)
         self.assertTemplateUsed(response, 'users/signup.html')
+
+class LoginTest(TestCase):
+
+    def test_use_signup_template(self):
+        """ Testing if """
+        response = self.client.get('/users/signup/')
+        print(response)
+        self.assertTemplateUsed(response, 'users/signup.html')
+
+class LogoutTest(TestCase):
+
+    def test_use_signup_template(self):
+        """ Testing if """
+        response = self.client.get('/users/signup/')
+        print(response)
+        self.assertTemplateUsed(response, 'users/signup.html')
