004f971334c86abf82b29505be01a08d6830b096
Eri Schon <erischon@gmail.com>
2021-03-15 10:49:58 +0100

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Categorie(models.Model):
+    cat_id = models.AutoField(primary_key=True)
+    cat_name = models.CharField(max_length=150)
+
+class Shops(models.Model):
+    shop_id = models.AutoField(primary_key=True)
+    shop_name = models.CharField(max_length=150)
+
+class Brand(models.Model):
+    brand_id = models.AutoField(primary_key=True)
+    brand_name = models.CharField(max_length=150)
+
+class Nutriscore(models.Model):
+    nut_id = models.AutoField(primary_key=True)
+    nut_type = models.CharField(max_length=1)
