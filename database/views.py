9d31f802e7ccc060861de8ccfbb0630c36e5295f
Eri Schon <erischon@gmail.com>
2021-03-17 10:13:45 +0100

    Update views.py

--- database/views.py
+++ database/views.py
@@ -3,5 +3,6 @@ from database.load import Load
 
 def etl(request):
     loading = Load()
-    message = loading.load_nutriscore()
+    # message = loading.load_nutriscore()
+    message = loading.load_data()
     return render(request, 'database/etl.html', {'message': message})
