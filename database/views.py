7ef7b933a723b0ff864e3e82314c906775d02f08
Eri Schon <erischon@gmail.com>
2021-03-18 09:43:32 +0100

    Update views.py

--- database/views.py
+++ database/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+
+from database.extract import Extract
+from database.transform import Transform
 from database.load import Load
 
 def etl(request):
+    return render(request, 'database/etl.html')
+
+def etl_extract(request):
+    extract = Extract()
+    message = extract.extract()
+    return render(request, 'database/etl.html', {'message_extract': message})
+
+def etl_transform(request):
+    transform = Transform()
+    message = transform.transform_basic()
+    return render(request, 'database/etl.html', {'message_transform': message})
+
+def etl_load(request):
     loading = Load()
     # message = loading.load_nutriscore()
     message = loading.load_data()
-    return render(request, 'database/etl.html', {'message': message})
+    return render(request, 'database/etl.html', {'message_load': message})
+
