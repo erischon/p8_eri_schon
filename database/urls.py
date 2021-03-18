7b054caff7413d06fbe6ee36014287a9b2089ce5
Eri Schon <erischon@gmail.com>
2021-03-18 09:43:29 +0100

    Update urls.py

--- database/urls.py
+++ database/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.etl, name='etl'), 
+    path('', views.etl, name='etl'),
+    path('extract', views.etl_extract, name='etl_extract'),
+    path('transform', views.etl_transform, name='etl_transform'),
+    path('load', views.etl_load, name='etl_load'),
 ]
