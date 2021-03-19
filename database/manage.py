358f9ed1c72798c7bc4827235e87f0ae5807fbe9
Eri Schon <erischon@gmail.com>
2021-03-19 09:04:50 +0100

    Create manage.py

---
+++ database/manage.py
@@ -0,0 +1,29 @@
+from django.db import connection, models
+from database.models import Product, Nutriscore, Categorie, Prodcat, Brand, Prodbrand, Shop, Prodshop
+
+class DBManage:
+
+    def __init__(self):
+        """ """
+        self.connection = connection.cursor()
+
+    def load_nutriscore(self):
+        """ I load the nutriscore and their id into the table. """
+        try:
+            query = "INSERT INTO database_nutriscore (nut_id, nut_type) VALUES (1, 'A'), (2, 'B'), (3, 'C'), (4, 'D'), (5, 'E')"
+            self.connection.execute(query)
+            message = "REUSSITE : Les différents Nutriscore ont été chargés dans la base."
+            return message
+
+        except Exception as ex:
+            return ex
+
+    def delete_tables(self):
+        Prodshop.objects.all().delete()
+        Prodbrand.objects.all().delete()
+        Prodcat.objects.all().delete()
+        Shop.objects.all().delete()
+        Brand.objects.all().delete()
+        Categorie.objects.all().delete()
+        Product.objects.all().delete()
+        return "Les Tables sont effacées."
