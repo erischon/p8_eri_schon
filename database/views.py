64bb3da222178844b123c0a19fb6fb4bfaaa4c39
Eri Schon <erischon@gmail.com>
2021-03-19 09:04:58 +0100

    Update views.py

--- database/views.py
+++ database/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from database.extract import Extract
 from database.transform import Transform
 from database.load import Load
+from database.manage import DBManage
 
 def etl(request):
     return render(request, 'database/etl.html')
@@ -19,7 +20,15 @@ def etl_transform(request):
 
 def etl_load(request):
     loading = Load()
-    # message = loading.load_nutriscore()
     message = loading.load_data()
     return render(request, 'database/etl.html', {'message_load': message})
 
+def etl_manage_nutriscore(request):
+    managing = DBManage()
+    message = managing.load_nutriscore()
+    return render(request, 'database/etl.html', {'message_manage_nutriscore': message})
+
+def etl_manage_delete(request):
+    managing = DBManage()
+    message = managing.delete_tables()
+    return render(request, 'database/etl.html', {'message_manage_delete': message})
