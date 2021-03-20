7f90efeb0e065cb79559acc4b240c3097abea4e3
Eri Schon <erischon@gmail.com>
2021-03-20 08:47:57 +0100

    Create etl.py

---
+++ database/management/commands/etl.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from database.manage import DBManage
+from database.extract import Extract
+from database.transform import Transform
+from database.load import Load
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        managing = DBManage()
+        managing.delete_tables()
+        extract = Extract()
+        extract.extract()
+        transform = Transform()
+        transform.transform_basic()
+        loading = Load()
+        loading.load_data()
