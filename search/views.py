f99bba17e9de09d8e3aa0514c84055af8f1569de
Eri Schon <erischon@gmail.com>
2021-03-22 10:58:31 +0100

    Update views.py

--- search/views.py
+++ search/views.py
@@ -4,6 +4,7 @@ from database.models import Product, Prodcat, Categorie
 def search_sub(request):
     query = "nutella"
     result_info = {}
+    categories = []
     result = Product.objects.filter(prod_name=query)[0]
 
     if not result:
@@ -11,8 +12,12 @@ def search_sub(request):
     else:
         prod_id = result.prod_id
         nutriscore = result.nut_id.nut_type
-        categorie = Prodcat.objects.filter(prod_id=result.prod_id)
+        
+        categorie_list = Prodcat.objects.filter(prod_id=result.prod_id)
+        for categorie in categorie_list:
+            categorie = categorie.cat_id.cat_name
+            categories.append(categorie)
 
-        result_info = {'prod_id': prod_id, 'nutriscore': nutriscore, 'categorie': categorie}
+        result_info = {'prod_id': prod_id, 'nutriscore': nutriscore, 'categories': categories}
 
     return render(request, 'search/results.html', {'results': result_info}) 
