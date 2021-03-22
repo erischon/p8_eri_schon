cb24531e5096902b89293f9068fe3544c9c17671
Eri Schon <erischon@gmail.com>
2021-03-22 09:48:26 +0100

    Update views.py

--- search/views.py
+++ search/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from database.models import Product
 
 def search_sub(request):
-    return render(request, 'search/results.html') 
+    result_list = {}
+    query = "nutella"
+    results = Product.objects.filter(prod_name=query)
+    if not results:
+        results = "Désolé, nous n'avons pas ce produit dans notre base de données."
+    else:
+        for result in results:
+            prod_id = results.values('prod_id')
+            nutriscore = results.values('nut_id')
+
+            result_list = {result: {'prod_id': prod_id, 'nutriscore': nutriscore}}
+
+    return render(request, 'search/results.html', {'results': result_list}) 
