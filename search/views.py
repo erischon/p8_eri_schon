5290db75f04d7d9ec56d1e8b356b98543e644105
Eri Schon <erischon@gmail.com>
2021-03-23 09:30:37 +0100

    Update views.py

--- search/views.py
+++ search/views.py
@@ -1,23 +1,12 @@
 from django.shortcuts import render
 from database.models import Product, Prodcat, Categorie
+from search.search import Search
 
 def search_sub(request):
     query = "nutella"
-    result_info = {}
-    categories = []
-    result = Product.objects.filter(prod_name=query)[0]
 
-    if not result:
-        result = "Désolé, nous n'avons pas ce produit dans notre base de données."
-    else:
-        prod_id = result.prod_id
-        nutriscore = result.nut_id.nut_type
-        
-        categorie_list = Prodcat.objects.filter(prod_id=result.prod_id)
-        for categorie in categorie_list:
-            categorie = categorie.cat_id.cat_name
-            categories.append(categorie)
+    search = Search()
+    search_product = search.find_product(query)
+    result_info = search.find_substitute(search_product)
 
-        result_info = {'prod_id': prod_id, 'nutriscore': nutriscore, 'categories': categories}
-
-    return render(request, 'search/results.html', {'results': result_info}) 
+    return render(request, 'search/results.html', {'results': result_info, 'product': search_product}) 
