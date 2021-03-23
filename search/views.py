7c9c12ebb919283a7484633529f707fe0d65d5ba
Eri Schon <erischon@gmail.com>
2021-03-23 10:12:28 +0100

    Update views.py

--- search/views.py
+++ search/views.py
@@ -4,9 +4,11 @@ from search.search import Search
 
 def search_sub(request):
     query = "nutella"
-
     search = Search()
+
     search_product = search.find_product(query)
+    product = search.product_infos(search_product)
+
     result_info = search.find_substitute(search_product)
 
-    return render(request, 'search/results.html', {'results': result_info, 'product': search_product}) 
+    return render(request, 'search/results.html', {'product': product, 'results': search_product}) 
