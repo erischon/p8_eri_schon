8d6db692b7c2bc81dac84f72cccbc29ef81929b8
Eri Schon <erischon@gmail.com>
2021-03-23 10:12:26 +0100

    Update search.py

--- search/search.py
+++ search/search.py
@@ -18,19 +18,41 @@ class Search:
             result = "Désolé, nous n'avons pas ce produit dans notre base de données."
             return result
         else:
+            # I take the first result
             result = result[0]
-            prod_id = result.prod_id
-            nutriscore = result.nut_id.nut_type 
-            categorie_list = Prodcat.objects.filter(prod_id=result.prod_id)     
-            for categorie in categorie_list:
-                categorie = categorie.cat_id.cat_name
-                self.categories.append(categorie)
-
-            self.result_info = {'prod_id': prod_id, 'nutriscore': nutriscore, 'categories': self.categories}
-            return self.result_info
+            return result
     
     def find_substitute(self, query):
-        pass
+        """ I'm the algorithm. """
+        result = query
+        result_info = {}
+        # categorie = Prodcat.objects.filter(prod_id=query.prod_id)[0]
+
+        # prod_list = Prodcat.objects.filter(cat_id=categorie.cat_id)
+        # prod_list = prod_list.prod_id
+
+        categorie_list = Prodcat.objects.filter(prod_id=result.prod_id)
+        for categorie in categorie_list:
+            categorie = categorie.cat_id.cat_name
+            self.categories.append(categorie)
+
+        prod_list = self.categories
+        return prod_list
+
+    def product_infos(self, object):
+        """ I extract the informations taht I need. """
+        result = object
+        product_info = {}
+        prod_id = result.prod_id
+        nutriscore = result.nut_id.nut_type 
+        categorie_list = Prodcat.objects.filter(prod_id=result.prod_id)     
+        for categorie in categorie_list:
+            categorie = categorie.cat_id.cat_name
+            self.categories.append(categorie)
+
+        product_info = {'prod_id': prod_id, 'nutriscore': nutriscore, 'categories': self.categories}
+        
+        return product_info
 
 
 if __name__ == "__main__":
