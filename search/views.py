a64131f61ae38cfe93520b1b36b5ea4892c5c321
Eri Schon <erischon@gmail.com>
2021-03-24 09:31:53 +0100

    Update views.py

--- search/views.py
+++ search/views.py
@@ -3,13 +3,17 @@ from database.models import Product, Prodcat, Categorie
 from search.search import Search
 
 def search_sub(request):
-    query = "nutella"
+    query = "Nutella"
     search = Search()
+    result_infos = []
 
     search_product = search.find_product(query)
     product = search.product_infos(search_product)
 
-    result_info = search.find_substitute(search_product)
-    result_infos = search.result_infos(result_info)
+    for categorie in product['categories']:
+        result_info = search.find_substitute(search_product)
+        result_infos.extend(search.result_infos(result_info))
+    
+    result_infos = [i for n, i in enumerate(result_infos) if i not in result_infos[n + 1:]]
 
     return render(request, 'search/results.html', {'product': product, 'results': result_infos}) 
