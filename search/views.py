5ddc8cc021bf6ad5e5ac0d199f93002ab6432601
Eri Schon <erischon@gmail.com>
2021-03-26 10:48:40 +0100

    Update views.py

--- search/views.py
+++ search/views.py
@@ -5,24 +5,24 @@ from database.models import Product, Prodcat, Categorie
 from search.search import Search
 from .forms import SearchForm
 
-def get_query(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = SearchForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
+def search_results(request):
+    search = Search()
+    query = request.GET['q']
+    result_infos = []
 
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = SearchForm()
-    return render(request, 'search/test.html', {'form': form})
+    search_product = search.find_product(query)
+    product = search.product_infos(search_product)
+
+    for categorie in product['categories']:
+        result_info = search.find_substitute(search_product)
+        result_infos.extend(search.result_infos(result_info))
+    
+    result_infos = [i for n, i in enumerate(result_infos) if i not in result_infos[n + 1:]]
+
+    return render(request, 'search/results.html', {'product': product, 'results': result_infos}) 
 
 def search_sub(request):
+    # query = request.GET['q']
     query = 'nutella'
     search = Search()
     result_infos = []
