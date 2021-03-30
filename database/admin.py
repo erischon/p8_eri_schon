dbc861aaab89252bff14eb549059f4f90563e561
Eri Schon <erischon@gmail.com>
2021-03-30 17:08:02 +0200

    Update admin.py

--- database/admin.py
+++ database/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from .models import Categorie, Shop, Brand, Nutriscore, Product, Myproduct, Prodbrand, Prodcat, Prodshop
+from .models import Categorie, Shop, Brand, Nutriscore, Product, Prodbrand, Prodcat, Prodshop
 
 admin.site.register(Categorie)
 admin.site.register(Shop)
 admin.site.register(Brand)
 admin.site.register(Nutriscore)
 admin.site.register(Product)
-admin.site.register(Myproduct)
 admin.site.register(Prodbrand)
 admin.site.register(Prodcat)
 admin.site.register(Prodshop)
