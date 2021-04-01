9903f3607db5215c1217458ad5b1108a46ce40c4
Eri Schon <erischon@gmail.com>
2021-04-01 16:32:06 +0200

    Update urls.py

--- search/urls.py
+++ search/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.search_sub, name='search_sub'),
     path('result/', views.search_results, name='search_results'),
+    path('prodinfos/<int:prod_id>', views.prodinfos, name="prodinfos"),
     path('saving/<int:product>', views.saving, name='saving'),
 ]
