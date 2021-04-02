a98b89edf0fac94cf018be658ac93722c57362a2
Eri Schon <erischon@gmail.com>
2021-04-02 12:40:27 +0200

    Update views.py

--- webapp/views.py
+++ webapp/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home_page(request):
     return render(request, 'webapp/home.html') 
+
+def mentions(request):
+    return render(request, 'webapp/mentions.html') 
