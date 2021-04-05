83d134315f5df7fe559f60f77503059072d9a7b1
Eri Schon <erischon@gmail.com>
2021-04-05 21:50:13 +0200

    Update test_views.py

--- search/tests/test_views.py
+++ search/tests/test_views.py
@@ -1,16 +1,23 @@
 from django.test import TestCase, Client
 from django.urls import reverse, resolve
 
-from database.models import Product
+from database.models import Product, Nutriscore, User
 
 class SearchTestViews(TestCase):
 
     def setUp(self):
         self.client = Client()
-        # self.product1 = Product.objects.get_or_create(prod_id=100)    
-        self.prodinfos_url = reverse('prodinfos', args=[1])
+
+        nutriscore = Nutriscore.objects.create(nut_id=1, nut_type="C")
+        self.product = Product.objects.create(
+            prod_id = 15,
+            prod_name = "test product",
+            nut_id = nutriscore,
+        )
+
+        self.prodinfos_url = reverse('prodinfos', args=[self.product.prod_id])
         self.search_sub_url = reverse('search_sub')
-        self.saving_url = reverse('saving', args=['8001505005592'])
+        # self.saving_url = reverse('saving', args=['8001505005592'])
 
     # def test_search_results(self):
     #     client = Client()
@@ -20,12 +27,12 @@ class SearchTestViews(TestCase):
     #     self.assertEquals(response.status_code, 200)
     #     self.assertTemplateUsed(response, 'search/results.html')
 
-    # def test_prodinfos(self):
+    def test_prodinfos(self):
 
-    #     response = self.client.get(self.prodinfos_url)
+        response = self.client.get(self.prodinfos_url)
 
-    #     self.assertEquals(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'search/prodinfos.html')
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'search/prodinfos.html')
 
     def test_search_sub(self):
 
@@ -34,9 +41,9 @@ class SearchTestViews(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'webapp/home.html')
 
-    def test_saving(self):
+    # def test_saving(self):
 
-        response = self.client.get(self.saving_url)
+    #     response = self.client.get(self.saving_url)
 
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'users/myproducts.html')
+    #     self.assertEquals(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'users/myproducts.html')
