47961842127c3a28d9ff9ad3da14c6a309e02278
Eri Schon <erischon@gmail.com>
2021-04-05 20:02:24 +0200

    Update test_views.py

--- search/tests/test_views.py
+++ search/tests/test_views.py
@@ -1,15 +1,42 @@
 from django.test import TestCase, Client
-from django.urls import reverse
-from database.models import Product, Prodcat, Categorie, User
+from django.urls import reverse, resolve
+
+from database.models import Product
 
 class SearchTestViews(TestCase):
 
-    def test_search_results(self):
-        client = Client()
+    def setUp(self):
+        self.client = Client()
+        # self.product1 = Product.objects.get_or_create(prod_id=100)    
+        self.prodinfos_url = reverse('prodinfos', args=[1])
+        self.search_sub_url = reverse('search_sub')
+        self.saving_url = reverse('saving', args=['8001505005592'])
 
-        response = client.get(reverse('search_results', args=['some-product']))
+    # def test_search_results(self):
+    #     client = Client()
+
+    #     response = client.get(reverse('search_results'))
+
+    #     self.assertEquals(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'search/results.html')
+
+    # def test_prodinfos(self):
+
+    #     response = self.client.get(self.prodinfos_url)
+
+    #     self.assertEquals(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'search/prodinfos.html')
+
+    def test_search_sub(self):
+
+        response = self.client.get(self.search_sub_url)
 
         self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'search/results.html')
+        self.assertTemplateUsed(response, 'webapp/home.html')
 
+    def test_saving(self):
 
+        response = self.client.get(self.saving_url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/myproducts.html')
