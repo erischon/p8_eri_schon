acf3c6ee93905390af3f3538bfaa02e06833c676
Eri Schon <erischon@gmail.com>
2021-04-05 11:42:05 +0200

    Update test_urls.py

---
+++ search/tests/test_urls.py
@@ -0,0 +1,22 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+from search.views import search_sub, search_results, prodinfos, saving
+
+class DatabaseTestUrls(SimpleTestCase):
+
+    def test_search_sub_url_is_resolved(self):
+        url = reverse('search_sub')
+        self.assertEqual(resolve(url).func, search_sub)
+
+    def test_search_results_url_is_resolved(self):
+        url = reverse('search_results')
+        self.assertEqual(resolve(url).func, search_results)
+
+    def test_prodinfos_url_is_resolved(self):
+        url = reverse('prodinfos', args=['1'])
+        self.assertEqual(resolve(url).func, prodinfos)
+
+    def test_saving_url_is_resolved(self):
+        url = reverse('saving', args=['1'])
+        self.assertEqual(resolve(url).func, saving)
