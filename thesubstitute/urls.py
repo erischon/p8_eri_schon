79d41d15b347d7c9738d99b51495b02a7e7f63cf
Eri Schon <erischon@gmail.com>
2021-04-05 12:46:40 +0200

    Update urls.py

--- thesubstitute/urls.py
+++ thesubstitute/urls.py
@@ -4,9 +4,8 @@ from webapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('webapp.urls')),
     path('database/', include('database.urls')),
-    path('', views.home_page, name='home'),
-    path('mentions/', views.mentions, name='mentions'),
     path('users/', include('users.urls')),
     path('search/', include('search.urls')),
 ]
