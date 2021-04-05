33cb4cce64f9fd6f1d1ceff18a37a7f8c37c55d5
Eri Schon <erischon@gmail.com>
2021-04-05 12:46:45 +0200

    Update test_urls.py

---
+++ webapp/tests/test_urls.py
@@ -0,0 +1,14 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+from webapp.views import home_page, mentions
+
+class WebappTestUrls(SimpleTestCase):
+
+    def test_home_url_is_resolved(self):
+        url = reverse('home')
+        self.assertEqual(resolve(url).func, home_page)
+
+    def test_mentions_url_is_resolved(self):
+        url = reverse('mentions')
+        self.assertEqual(resolve(url).func, mentions)
