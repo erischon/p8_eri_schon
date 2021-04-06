7846ca21aecd7615f73d90ea47d97fcf36a31830
Eri Schon <erischon@gmail.com>
2021-04-06 18:11:41 +0200

    Update test_views.py

---
+++ webapp/tests/test_views.py
@@ -0,0 +1,23 @@
+from django.test import TestCase, Client
+from django.urls import reverse, resolve
+
+class WebAppTestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+        self.homepage_url = reverse('home')
+        self.mentions_url = reverse('mentions')
+
+
+    def test_homepage_view(self):
+        response = self.client.get(self.homepage_url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'webapp/home.html')
+
+    def test_mentions_view(self):
+        response = self.client.get(self.mentions_url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'webapp/mentions.html')
