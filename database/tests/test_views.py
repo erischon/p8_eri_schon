b021a12e24f66d3fb13fa91993abf83ac987c11c
Eri Schon <erischon@gmail.com>
2021-04-06 11:16:50 +0200

    Update test_views.py

--- database/tests/test_views.py
+++ database/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 
-class DatabaseTestViews(testcase):
+class DatabaseTestViews(TestCase):
 
     def test_etl(self):
         pass
