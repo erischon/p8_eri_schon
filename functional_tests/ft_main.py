d5a1fd8c6515343334933fbdda79948273b2a77c
Eri Schon <erischon@gmail.com>
2021-04-07 10:33:40 +0200

    Update ft_main.py

--- functional_tests/ft_main.py
+++ functional_tests/ft_main.py
@@ -2,18 +2,22 @@ from selenium import webdriver
 import unittest
 
 from ft_home import NewVisitorTest
-from ft_users import NewUser
+# from ft_search import NewSearch
+# from ft_users import NewUser
 
 class FtMain:
 
     def __init__(self):
         self.ft_home = NewVisitorTest()
-        self.ft_users = NewUser()
+        # self.ft_users = NewUser()
+        # self.ft_search = NewSearch()
         self.test = unittest.main(warnings='ignore')
 
     def functional_tests(self):
         print("Tests Fonctionnels pour la HP :")
         self.ft_home(self.test)
+        # self.ft_search(self.test)
+        # self.ft_users(self.test)
 
 
 if __name__ == "__main__":
