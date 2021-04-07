35daabbe54fde3cc52a964f0b9f2457d2d33497f
Eri Schon <erischon@gmail.com>
2021-04-07 16:31:19 +0200

    Update urls.py

--- thesubstitute/urls.py
+++ thesubstitute/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from webapp import views
 
 urlpatterns = [
@@ -9,3 +9,6 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('search/', include('search.urls')),
 ]
+
+handler404 = 'webapp.views.error_404'
+handler400 = 'webapp.views.error_404'
