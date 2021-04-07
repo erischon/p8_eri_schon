c7b35e47ca999ad45357f7ba40f5d14ef143a6d6
Eri Schon <erischon@gmail.com>
2021-04-07 12:33:08 +0200

    Delete _tests.py

--- webapp/_tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-class HomePageTest(TestCase):
-
-    def test_uses_hp_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'webapp/home.html')
