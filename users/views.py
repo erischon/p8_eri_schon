374fd8dd127554fcec105cdc9e5c26ce64de6321
Eri Schon <erischon@gmail.com>
2021-04-07 12:33:04 +0200

    Update views.py

--- users/views.py
+++ users/views.py
@@ -4,23 +4,25 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
+
 from database.models import Product
+from users.forms import SignUpForm
 
 def signupuser(request):
     ''' '''
     if request.method == 'POST':
         if request.POST['password1'] == request.POST['password2']:
             try:
-                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'], email=request.POST['email'])
                 user.save()
                 login(request, user)
                 return redirect('moncompte')
             except IntegrityError:
-                return render(request, 'users/signup.html', {'form':UserCreationForm(), 'error':'Cet email est déjà pris. Merci d\'en choisir un nouveau.'})
+                return render(request, 'users/signup.html', {'form':SignUpForm(), 'error':'Cet email est déjà pris. Merci d\'en choisir un nouveau.'})
         else:
-            return render(request, 'users/signup.html', {'form':UserCreationForm(), 'error':'Les mots de passe ne sont pas identiques.'})
+            return render(request, 'users/signup.html', {'form':SignUpForm(), 'error':'Les mots de passe ne sont pas identiques.'})
     else:
-        return render(request, 'users/signup.html', {'form':UserCreationForm()})
+        return render(request, 'users/signup.html', {'form':SignUpForm()})
 
 def loginuser(request):
     ''' '''
