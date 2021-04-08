112ae7bc92ecffb11b8820784525ca68a6e626f9
Eri Schon <erischon@gmail.com>
2021-04-08 15:53:34 +0200

    Update etl.py

--- database/management/commands/etl.py
+++ database/management/commands/etl.py
@@ -1,6 +1,6 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
-from database.manage import DBManage
+from database.manage import DBManage # Can be remove if we don't want to delete
 from database.extract import Extract
 from database.transform import Transform
 from database.load import Load
@@ -9,8 +9,8 @@ from database.load import Load
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-        managing = DBManage()
-        # managing.delete_tables()
+        managing = DBManage() # Can be remove if we don't want to delete
+        managing.delete_tables() # Can be remove if we don't want to delete
         extract = Extract()
         extract.extract()
         transform = Transform()
