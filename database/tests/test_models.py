ac96cf4fb7a713717f6e7c8e55eaf8a44e675929
Eri Schon <erischon@gmail.com>
2021-04-10 16:32:12 +0200

    Update test_models.py

---
+++ database/tests/test_models.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+
+from database.models import Product, Nutriscore
+
+class AuthorModelTest(TestCase):
+
+    def setUp(self):
+        nutriscore = Nutriscore.objects.create(nut_id=1, nut_type="C")
+        self.product = Product.objects.create(
+            prod_id=3017620422003,
+            prod_name="test product",
+            nut_id=nutriscore,
+        )
+
+    def test_first_name_label(self):
+        product = Product.objects.get(prod_id=3017620422003)
+
+        field_label = product._meta.get_field('prod_name').verbose_name
+        max_length = product._meta.get_field('prod_name').max_length
+
+        self.assertEquals(field_label, 'prod name')
+        self.assertEquals(max_length, 250)
