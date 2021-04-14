16a6ce40611484030dfcd4368dd9d5779cd0d67a
Eri Schon <erischon@gmail.com>
2021-04-14 10:24:12 +0200

    Update etl.py

--- database/management/commands/etl.py
+++ database/management/commands/etl.py
@@ -12,11 +12,15 @@ class Command(BaseCommand):
     '''
 
     def handle(self, *args, **kwargs):
+        self.stdout.write('Launching the ETL :')
         managing = DBManage()  # Can be remove if we don't want to delete
         managing.delete_tables()  # Can be remove if we don't want to delete
+        self.stdout.write('Start by Extracting...')
         extract = Extract()
         extract.extract()
+        self.stdout.write('Now Transforming...')
         transform = Transform()
         transform.transform_basic()
+        self.stdout.write('And finaly Loading...')
         loading = Load()
         loading.load_data()
